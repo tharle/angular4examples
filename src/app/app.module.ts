cab6576443b2d7a7c4684feed0edc62cb60ba231
tharle <tharlejc@gmail.com>
2018-07-20 15:08:10 -0400

    Presque fini les exercices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule} from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -16,12 +16,8 @@ import { ProductModule } from './products/product.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ]),
-    ProductModule
+    ProductModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,29 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductDetailGuard } from './product-detail.guard';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe'
-import { StartComponent } from '../shared/star.component';
+import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule, 
-    FormsModule,
-    RouterModule.forChild([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', 
-        component: ProductDetailComponent,
-        canActivate: [ProductDetailGuard]},
-    ])
+    SharedModule,
+    ProductRoutingModule
   ],
   declarations: [
   	ProductListComponent,
-    ConvertToSpacesPipe,
-    StartComponent,
     ProductDetailComponent,
   ]
 })
